ac02b3e0e340c3572fad98fe734919fcc3305c5b
Yuri <yuri@rawbw.com>
2019-02-13 22:28:36 -0800

    Remove unnecessary lines from main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
 
-#include <array>
-#include <vector>
 #include <iostream>
-#include <fstream>
-#include <sstream>
 
-#include <stdio.h>
-#include <assert.h>
-
-#include "Vec3.h"
-#include "Mat3.h"
-#include "molecule.h"
 #include "xerror.h"
 #include "common.h"
 #include "js-binding.h"
@@ -19,34 +9,18 @@
 
 #include <mujs.h>
 
-/*
-class MoleculeIterator {
-public:
-  template<typename Fn>
-  static void direct(Molecule &m, Fn &&fn) {
-    for (auto it = m.atoms.begin(), ite = m.atoms.end(); it != ite; it++)
-      if (!fn(*it))
-        break;
-  }
-  template<typename Fn>
-  static void reverse(Molecule &m, Fn &&fn) {
-    for (auto it = m.atoms.rbegin(), ite = m.atoms.rend(); it != ite; it++)
-      if (!fn(*it))
-        break;
-  }
-}; // MoleculeIterator
-*/
-
 static void printTime(std::time_t tm, const char *when) {
   std::cout << "-- " << PROGRAM_NAME << " is " << when << " at " << ctime(&tm); // ctime contains EOL
 }
 
 static int main_guarded(int argc, char* argv[]) {
+  // arguments parsing
   if (argc < 2) {
     std::cerr << "expect an argument: {xyz-file}" << std::endl;
     return 1;
   }
 
+  // time printouts
   printTime(Tm::start(), "starting");
   Util::OnExit doPrintTime([](){
     auto tmFinish = Tm::now();
@@ -55,16 +29,10 @@ static int main_guarded(int argc, char* argv[]) {
   });
 
   // run JavaScript, everything else is done from there
-  if (1) {
-    js_State *J = js_newstate(NULL, NULL, JS_STRICT);
-
-    JsBinding::registerFunctions(J);
-
-    js_dofile(J, argv[1]);
-
-    js_freestate(J);
-    return 0;
-  }
+  js_State *J = js_newstate(NULL, NULL, JS_STRICT);
+  JsBinding::registerFunctions(J);
+  js_dofile(J, argv[1]);
+  js_freestate(J);
 
   return 0;
 }
