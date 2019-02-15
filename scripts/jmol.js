30c8565f7a18d7dc9c00b0acf6310ef71a44d762
Yuri <yuri@rawbw.com>
2019-02-14 22:30:09 -0800

    Add Jmol.renderMoleculesToFiles, improve the code in Jmol

--- scripts/jmol.js
+++ scripts/jmol.js
@@ -3,8 +3,18 @@
 var imageFormat = 'PNG'
 var deftImageSz = [800, 600]
 
-function renderMoleculeToFile(m, params) {
-  // image size
+function scriptXyzToPngInstruction(xyzFile, pngFile, sz) {
+  return "load '"+xyzFile.fname()+"';\n"+
+         "write IMAGE "+sz[0]+" "+sz[1]+" "+imageFormat+" '"+pngFile.fname()+"';\n"
+}
+
+function runJmoldata(scriptFile) {
+  print("> running jmoldata")
+  system("jmoldata -s "+scriptFile.fname())
+  print("< running jmoldata")
+}
+
+function paramsToSz(params) {
   var szx, szy
   if (params != undefined && params.szX != undefined && params.szY != undefined) {
     szx = params.szX
@@ -13,19 +23,43 @@ function renderMoleculeToFile(m, params) {
     szx = deftImageSz[0]
     szy = deftImageSz[1]
   }
+  return [szx, szy]
+}
+
+function renderMoleculeToFile(m, params) {
+  // image size
+  var sz = paramsToSz(params)
   // save molecule as xyz
   var xyz = new TempFile("xyz")
   writeXyzFile(m, xyz.fname())
   // prepare files
   var output = new TempFile("png")
-  var script = new TempFile("jmol-script",
-    "load '"+xyz.fname()+"';\n"+
-    "write IMAGE "+szx+" "+szy+" "+imageFormat+" '"+output.fname()+"';\n"
-  )
+  var script = new TempFile("jmol-script", scriptXyzToPngInstruction(xyz, output, sz))
   // run jmoldata command
-  system("jmoldata -s "+script.fname())
+  runJmoldata(script)
   //
   return output
+}
+
+function renderMoleculesToFiles(molecules, params) {
+  // image size
+  var sz = paramsToSz(params)
+  // save molecules as xyz files, create output files, create Jmol script
+  var xyzFiles = []
+  var pngFiles = []
+  var script = ""
+  for (var i = 0; i < molecules.length; i++) {
+    var xyzFile = new TempFile("xyz")
+    writeXyzFile(molecules[i], xyzFile.fname())
+    xyzFiles.push(xyzFile)
+    var pngFile = new TempFile("png")
+    pngFiles.push(pngFile)
+    script += scriptXyzToPngInstruction(xyzFile, pngFile, sz)
+  }
+  // run jmoldata command
+  runJmoldata(new TempFile("jmol-script", script))
+  //
+  return pngFiles
 }
 
 function renderMoleculeToMemory(m, params) {
@@ -33,5 +67,6 @@ function renderMoleculeToMemory(m, params) {
 }
 
 exports.renderMoleculeToFile = renderMoleculeToFile
+exports.renderMoleculesToFiles = renderMoleculesToFiles
 exports.renderMoleculeToMemory = renderMoleculeToMemory
 
