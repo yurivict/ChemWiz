d96a0d153e546bd774c2633d353adc3ef3a570e7
Yuri <yuri@rawbw.com>
2019-02-14 22:25:39 -0800

    Fix formatting in process.cpp

--- process.cpp
+++ process.cpp
@@ -1,4 +1,5 @@
 #include "process.h"
+
 #include <string>
 #include <array>
 #include <memory>
@@ -9,9 +10,11 @@ std::string Process::exec(const std::string &cmd) {
   std::array<char, 128> buffer;
   std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
   if (!pipe)
-  throw std::runtime_error("popen() failed!");
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+    throw std::runtime_error("popen() failed!");
+
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
     result += buffer.data();
+
   return result;
 }
 
