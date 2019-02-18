5ec34269d59d463890c0dfb3488cc020e0b1cd0e
Yuri <yuri@rawbw.com>
2019-02-18 01:20:56 -0800

    Add the Url module with the pdbMmtfGzipped function in it

---
+++ scripts/url.js
@@ -0,0 +1,6 @@
+// module Url: list of various relevant URLs that can be used from ChemWiz
+
+exports.pdbMmtfGzipped = function(pdbId) {
+  return "https://mmtf.rcsb.org/v1.0/full/"+pdbId+".mmtf.gz"
+}
+
