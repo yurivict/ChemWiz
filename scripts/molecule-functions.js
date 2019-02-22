3f8d148b86a1b1663399efec8083443bb973e9c8
Yuri <yuri@rawbw.com>
2019-02-21 22:09:45 -0800

    Add 'printBondLengths' function in the 'MoleculeFunctions' module

--- scripts/molecule-functions.js
+++ scripts/molecule-functions.js
@@ -18,3 +18,16 @@ exports.rmsd = function(m1, m2) {
   return rmsd(extractCoords(m1), extractCoords(m2))
 }
 
+exports.printBondLengths = function(m) {
+  var aa = m.getAtoms()
+  for (var a = 0; a < aa.length; a++) {
+    var atom = aa[a]
+    print("- atom#"+(a+1)+": elt="+atom.getElement()+" pos="+atom.getPos())
+    var bonds = atom.getBonds()
+    for (var b = 0; b < bonds.length; b++) {
+      var bondAtom = bonds[b]
+      print(" ---> bond#"+(b+1)+": elt="+bondAtom.getElement()+" pos="+bondAtom.getPos()+" bondLength="+vecLength(vecMinus(atom.getPos(),bondAtom.getPos())))
+    }
+  }
+}
+
