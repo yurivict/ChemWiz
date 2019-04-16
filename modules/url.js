032aafa492a5510e47ae018fdd67f3dd2f4d8000
Yuri <yuri@rawbw.com>
2019-04-16 13:16:34 -0700

    Add pdb-formatted URLs.

--- modules/url.js
+++ modules/url.js
@@ -1,5 +1,13 @@
 // module Url: list of various relevant URLs that can be used from ChemWiz
 
+exports.pdbPdbGzipped = function(pdbId) {
+  return "https://files.rcsb.org/download/"+pdbId.toLowerCase()+".pdb.gz"
+}
+
+exports.pdbPdb = function(pdbId) {
+  return "https://files.rcsb.org/download/"+pdbId.toLowerCase()+".pdb"
+}
+
 exports.pdbMmtfGzipped = function(pdbId) {
   return "https://mmtf.rcsb.org/v1.0/full/"+pdbId+".mmtf.gz"
 }
