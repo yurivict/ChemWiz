3873853b8f1d661aea227c81178575e06fc15f6d
Yuri <yuri@rawbw.com>
2019-05-20 14:42:15 -0700

    Make the QA-running code more organized, add comments, functions, etc.

--- qa/run-all-tests.js
+++ qa/run-all-tests.js
@@ -1,3 +1,17 @@
+// run-all-tests.js: runs all QA tests and reports the results
+
+// the list of cases
+
+var all_tests = ["xyz",
+                 "vec3-ops", "vec3-rmsd",
+                 "mat3-ops", "mat3-rotate",
+                 "computeConvexHullFacets", "computeConvexHullFacets+furthestdist",
+                 "web-ui-http", "web-ui-https", "web-ui-url",
+                 "gzip", "mmtf",
+                 "calc-erkale", "calc-nwchem"
+                ]
+
+// helper functions
 
 function printStatus(res, tmStartSec, tmEndSec) {
   if (res == "OK")
@@ -14,39 +28,38 @@ function printStatus(res, tmStartSec, tmEndSec) {
   }
 }
 
-// run-all-tests.js: runs all tests
+// run the QA cases
+function RunAllTests() {
+  var nSucc = 0
+  var nFail = 0
+  var tmAllStartSec = Time.now()
+  for (var t = 0; t < all_tests.length; t++) {
+    printn("running test: "+all_tests[t]+" ... ")
+    flush()
+    var Test = require('qa/'+all_tests[t])
+    var tmCaseStartSec = Time.now()
+    var res = Test.run()
+    var tmCaseEndSec = Time.now()
+    printStatus(res, tmCaseStartSec, tmCaseEndSec)
+    if (res == "OK")
+      nSucc++
+    else if (res == "FAIL")
+      nFail++
+    else
+      throw "Invalid test output '"+res+"' for the test '"+all_tests[t]+"'"
+  }
+  var tmAllEndSec = Time.now()
 
-var all_tests = ["xyz",
-                 "vec3-ops", "vec3-rmsd",
-                 "mat3-ops", "mat3-rotate",
-                 "computeConvexHullFacets", "computeConvexHullFacets+furthestdist",
-                 "web-ui-http", "web-ui-https", "web-ui-url",
-                 "gzip", "mmtf",
-                 "calc-erkale", "calc-nwchem"
-                ]
-
-var nSucc = 0
-var nFail = 0
-var tmAllStartSec = Time.now()
-for (var t = 0; t < all_tests.length; t++) {
-  printn("running test: "+all_tests[t]+" ... ")
-  flush()
-  var Test = require('qa/'+all_tests[t])
-  var tmCaseStartSec = Time.now()
-  var res = Test.run()
-  var tmCaseEndSec = Time.now()
-  printStatus(res, tmCaseStartSec, tmCaseEndSec)
-  if (res == "OK")
-    nSucc++
-  else if (res == "FAIL")
-    nFail++
+  // print the QA status
+  if (nSucc == all_tests.length)
+    printna(["clr.fg.green"], "All "+all_tests.length+" tests succeeded")
   else
-    throw "Invalid test output '"+res+"' for the test '"+all_tests[t]+"'"
+    printna(["clr.fg.red"], nFail+" test(s) failed out of "+all_tests.length+" total tests")
+  printa(["clr.fg.reset"], " (in "+(tmAllEndSec-tmAllStartSec)+" secs)")
 }
-var tmAllEndSec = Time.now()
 
-if (nSucc == all_tests.length)
-  printna(["clr.fg.green"], "All "+all_tests.length+" tests succeeded")
-else
-  printna(["clr.fg.red"], nFail+" test(s) failed out of "+all_tests.length+" total tests")
-printa(["clr.fg.reset"], " (in "+(tmAllEndSec-tmAllStartSec)+" secs)")
+/// MAIN
+
+RunAllTests()
+
+
