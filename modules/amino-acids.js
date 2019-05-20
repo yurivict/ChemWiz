5eeda6076d5f6296a3c6c962f156259e1b0fe9c3
Yuri <yuri@rawbw.com>
2019-05-20 00:05:45 -0700

    Add anglesArrayToObject() and anglesArraysToObjects() convenience functions to the AminoAcids JS module.

--- modules/amino-acids.js
+++ modules/amino-acids.js
@@ -44,7 +44,47 @@ function combine(peptide, angles) {
   return peptideChain
 }
 
+//
+// convenience function that converts the amino acid backbone angles to the object, mostly for the ease of debugging
+//
+function anglesArrayToObject(angles) {
+  var o = {}
+  for (var i = 0; i < angles.length; i++) {
+    switch (i) {
+      case 0:   o.omega    = angles[0]; break;
+      case 1:   o.phi      = angles[1]; break;
+      case 2:   o.psi      = angles[2]; break;
+      case 3:   o.adjN     = angles[3]; break;
+      case 4:   o.adjCmain = angles[4]; break;
+      case 5:   o.adjCoo   = angles[5]; break;
+      case 6:   o.O2Rise   = angles[6]; break;
+      case 7:   o.O2Tilt   = angles[7]; break;
+      case 8:   o.PlRise   = angles[8]; break;
+      case 9:   o.PlTilt   = angles[9]; break;
+      default:
+        throw "invalid index "+i+" in the angles array"
+    }
+  }
+  return o
+}
+
+//
+// convenience function that converts the array of amino acid backbone angles to the objects, mostly for the ease of debugging
+//
+function anglesArraysToObjects(angleArrays) {
+  var a = []
+  for (var i = 0; i < angleArrays.length; i++)
+    a.push(anglesArrayToObject(angleArrays[i]))
+  return a
+}
+
+//
+// exports
+//
+
 exports.codeToName = codeToName
 exports.codeToFile = codeToFile
 exports.decodePeptide = decodePeptide
 exports.combine = combine
+exports.anglesArrayToObject = anglesArrayToObject
+exports.anglesArraysToObjects = anglesArraysToObjects
