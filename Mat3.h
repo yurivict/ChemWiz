0d29e24709fe22895c760d0f35e0d02580636263
Yuri <yuri@rawbw.com>
2019-06-01 17:27:28 -0700

    Templetize Vec3 and Mat3 classes into TVec/TMat3 templates.

--- Mat3.h
+++ Mat3.h
@@ -7,16 +7,17 @@
 
 typedef double Float;
 
-class Mat3 : public boost::array<Vec3, 3> {
+template<typename Float>
+class TMat3 : public boost::array<TVec3<Float>, 3> {
 public:
-  Mat3() { }
-  Mat3(const Vec3 &v) : boost::array<Vec3, 3>({{Vec3(v(X),0,0), Vec3(0,v(Y),0), Vec3(0,0,v(Z))}}) { } // Vec3 -> diagonal matrix Mat3 constructor
-  Mat3(const Vec3 &v1, const Vec3 &v2, const Vec3 &v3) : boost::array<Vec3, 3>({{v1, v2, v3}}) { }
+  TMat3() { }
+  TMat3(const TVec3<Float> &v) : boost::array<Vec3, 3>({{Vec3(v(X),0,0), Vec3(0,v(Y),0), Vec3(0,0,v(Z))}}) { } // Vec3 -> diagonal matrix TMat3 constructor
+  TMat3(const TVec3<Float> &v1, const Vec3 &v2, const Vec3 &v3) : boost::array<Vec3, 3>({{v1, v2, v3}}) { }
   Float& operator()(unsigned idx1, unsigned idx2) { // 1-based index access
     return (*this)[idx1-1](idx2);
   }
-  Mat3 t() const {
-    auto swap = [](Vec3::val_t &f1, Vec3::val_t &f2) {
+  TMat3 t() const {
+    auto swap = [](typename TVec3<Float>::val_t &f1, typename Vec3::val_t &f2) {
       auto tmp = f1;
       f1 = f2;
       f2 = tmp;
@@ -27,62 +28,65 @@ public:
     swap(tmp(2,3), tmp(3,2));
     return tmp;
   }
-  static bool almostEquals(const Mat3 &m1, const Mat3 &m2, Float eps)
-    {return Vec3::almostEquals(m1[0], m2[0], eps) && Vec3::almostEquals(m1[1], m2[1], eps) && Vec3::almostEquals(m1[2], m2[2], eps);}
-  static Mat3 zero() {return Mat3(Vec3(0,0,0), Vec3(0,0,0), Vec3(0,0,0));}
-  static Mat3 identity() {return Mat3(Vec3(1,0,0), Vec3(0,1,0), Vec3(0,0,1));}
-  Mat3 transpose() const {
+  static bool almostEquals(const TMat3 &m1, const TMat3 &m2, Float eps)
+    {return TVec3<Float>::almostEquals(m1[0], m2[0], eps) && Vec3::almostEquals(m1[1], m2[1], eps) && Vec3::almostEquals(m1[2], m2[2], eps);}
+  static TMat3 zero() {return TMat3(TVec3<Float>(0,0,0), Vec3(0,0,0), Vec3(0,0,0));}
+  static TMat3 identity() {return TMat3(TVec3<Float>(1,0,0), Vec3(0,1,0), Vec3(0,0,1));}
+  TMat3 transpose() const {
     const auto &i = *this;
-    return Mat3(Vec3(i(1,1),i(2,1),i(3,1)), Vec3(i(1,2),i(2,2),i(3,2)), Vec3(i(1,3),i(2,3),i(3,3)));
+    return TMat3(TVec3<Float>(i(1,1),i(2,1),i(3,1)), Vec3(i(1,2),i(2,2),i(3,2)), Vec3(i(1,3),i(2,3),i(3,3)));
   }
-  static Mat3 rotate(const Vec3 &r) {
+  static TMat3 rotate(const TVec3<Float> &r) {
     Float rLen = r.len();
     if (rLen == 0)
-      return Mat3::identity();
+      return TMat3::identity();
     return rotate(r/rLen, rLen);
   }
-  static Mat3 rotate(const Vec3 &u, Float theta) {
+  static TMat3 rotate(const TVec3<Float> &u, Float theta) {
     Float sinTh;
     Float cosTh;
     sincos(theta, &sinTh, &cosTh);
-    return Mat3(
-      Vec3(cosTh+u(1)*u(1)*(1-cosTh),      u(1)*u(2)*(1-cosTh)-u(3)*sinTh, u(1)*u(3)*(1-cosTh)+u(2)*sinTh),
-      Vec3(u(2)*u(1)*(1-cosTh)+u(3)*sinTh, cosTh+u(2)*u(2)*(1-cosTh),      u(2)*u(3)*(1-cosTh)-u(1)*sinTh),
-      Vec3(u(3)*u(1)*(1-cosTh)-u(2)*sinTh, u(3)*u(2)*(1-cosTh)+u(1)*sinTh, cosTh+u(3)*u(3)*(1-cosTh))
+    return TMat3(
+      TVec3<Float>(cosTh+u(1)*u(1)*(1-cosTh),      u(1)*u(2)*(1-cosTh)-u(3)*sinTh, u(1)*u(3)*(1-cosTh)+u(2)*sinTh),
+      TVec3<Float>(u(2)*u(1)*(1-cosTh)+u(3)*sinTh, cosTh+u(2)*u(2)*(1-cosTh),      u(2)*u(3)*(1-cosTh)-u(1)*sinTh),
+      TVec3<Float>(u(3)*u(1)*(1-cosTh)-u(2)*sinTh, u(3)*u(2)*(1-cosTh)+u(1)*sinTh, cosTh+u(3)*u(3)*(1-cosTh))
     );
   }
-  Vec3 toRotationVector() const {
+  TVec3<Float> toRotationVector() const {
     assert(0); // TODO
   }
   Float operator()(unsigned idx1, unsigned idx2) const { // 1-based index access
     return (*this)[idx1-1](idx2);
   }
-  friend std::ostream& operator<<(std::ostream &os, const Mat3 &m) {
+  friend std::ostream& operator<<(std::ostream &os, const TMat3 &m) {
     os << "{" << m[0] << "," << m[1] << "," << m[2] << "}";
     return os;
   }
-  Mat3 operator+(const Mat3 &m) const {
-    return Mat3((*this)[0]+m[0], (*this)[1]+m[1], (*this)[2]+m[2]);
+  TMat3 operator+(const TMat3 &m) const {
+    return TMat3((*this)[0]+m[0], (*this)[1]+m[1], (*this)[2]+m[2]);
   }
-  Mat3 operator-(const Mat3 &m) const {
-    return Mat3((*this)[0]-m[0], (*this)[1]-m[1], (*this)[2]-m[2]);
+  TMat3 operator-(const TMat3 &m) const {
+    return TMat3((*this)[0]-m[0], (*this)[1]-m[1], (*this)[2]-m[2]);
   }
-  Mat3 operator*(Float m) const {
-    return Mat3((*this)[0]*m, (*this)[1]*m, (*this)[2]*m);
+  TMat3 operator*(Float m) const {
+    return TMat3((*this)[0]*m, (*this)[1]*m, (*this)[2]*m);
   }
-  Mat3 operator*(const Mat3 &m) const {
+  TMat3 operator*(const TMat3 &m) const {
     const auto &i = *this;
-    return Mat3(Vec3(i(1,1)*m(1,1)+i(1,2)*m(2,1)+i(1,3)*m(3,1), i(1,1)*m(1,2)+i(1,2)*m(2,2)+i(1,3)*m(3,2), i(1,1)*m(1,3)+i(1,2)*m(2,3)+i(1,3)*m(3,3)),
-                Vec3(i(2,1)*m(1,1)+i(2,2)*m(2,1)+i(2,3)*m(3,1), i(2,1)*m(1,2)+i(2,2)*m(2,2)+i(2,3)*m(3,2), i(2,1)*m(1,3)+i(2,2)*m(2,3)+i(2,3)*m(3,3)),
-                Vec3(i(3,1)*m(1,1)+i(3,2)*m(2,1)+i(3,3)*m(3,1), i(3,1)*m(1,2)+i(3,2)*m(2,2)+i(3,3)*m(3,2), i(3,1)*m(1,3)+i(3,2)*m(2,3)+i(3,3)*m(3,3)));
+    return TMat3(TVec3<Float>(i(1,1)*m(1,1)+i(1,2)*m(2,1)+i(1,3)*m(3,1), i(1,1)*m(1,2)+i(1,2)*m(2,2)+i(1,3)*m(3,2), i(1,1)*m(1,3)+i(1,2)*m(2,3)+i(1,3)*m(3,3)),
+                 TVec3<Float>(i(2,1)*m(1,1)+i(2,2)*m(2,1)+i(2,3)*m(3,1), i(2,1)*m(1,2)+i(2,2)*m(2,2)+i(2,3)*m(3,2), i(2,1)*m(1,3)+i(2,2)*m(2,3)+i(2,3)*m(3,3)),
+                 TVec3<Float>(i(3,1)*m(1,1)+i(3,2)*m(2,1)+i(3,3)*m(3,1), i(3,1)*m(1,2)+i(3,2)*m(2,2)+i(3,3)*m(3,2), i(3,1)*m(1,3)+i(3,2)*m(2,3)+i(3,3)*m(3,3)));
   }
-  Vec3 operator*(const Vec3 &v) const {
+  TVec3<Float> operator*(const Vec3 &v) const {
     const auto &i = *this;
-    return Vec3(i(1,1)*v(1) + i(1,2)*v(2) + i(1,3)*v(3),
-                i(2,1)*v(1) + i(2,2)*v(2) + i(2,3)*v(3),
-                i(3,1)*v(1) + i(3,2)*v(2) + i(3,3)*v(3));
+    return TVec3<Float>(i(1,1)*v(1) + i(1,2)*v(2) + i(1,3)*v(3),
+                        i(2,1)*v(1) + i(2,2)*v(2) + i(2,3)*v(3),
+                        i(3,1)*v(1) + i(3,2)*v(2) + i(3,3)*v(3));
   }
-  Mat3 operator/(Float m) const {
-    return Mat3((*this)[0]/m, (*this)[1]/m, (*this)[2]/m);
+  TMat3 operator/(Float m) const {
+    return TMat3((*this)[0]/m, (*this)[1]/m, (*this)[2]/m);
   }
-}; // Mat3
+}; // TMat3
+
+typedef TMat3<double> Mat3;
+typedef TMat3<float> Mat3f;
--- Vec3.h
+++ Vec3.h
@@ -9,47 +9,48 @@ typedef double Float;
 /// Vector helper classes
 enum VecCoords {X=1, Y=2, Z=3};
 
-class Vec3 : public boost::array<Float, 3> {
+template<typename Float>
+class TVec3 : public boost::array<Float, 3> {
 public:
   typedef Float val_t;
-  Vec3() { }
-  Vec3(Float x, Float y, Float z) : boost::array<Float, 3>({{x, y, z}}) { }
+  TVec3() { }
+  TVec3(Float x, Float y, Float z) : boost::array<Float, 3>({{x, y, z}}) { }
   Float& operator()(unsigned idx) { // 1-based index access
     return (*this)[idx-1];
   }
   Float operator()(unsigned idx) const { // 1-based index access
     return (*this)[idx-1];
   }
-  static Vec3 zero() {return Vec3(0,0,0);}
-  static Vec3 one(unsigned idx, Float val) {Vec3 vec(0,0,0); vec(idx) = val; return vec;}
-  static bool almostEquals(const Vec3 &v1, const Vec3 &v2, Float eps)
+  static TVec3 zero() {return TVec3(0,0,0);}
+  static TVec3 one(unsigned idx, Float val) {TVec3 vec(0,0,0); vec(idx) = val; return vec;}
+  static bool almostEquals(const TVec3 &v1, const TVec3 &v2, Float eps)
     {return almostEquals(v1(X),v2(X),eps) && almostEquals(v1(Y),v2(Y),eps) && almostEquals(v1(Z),v2(Z),eps);}
   static bool almostEquals(Float c1, Float c2, Float eps)
     {return std::fabs(c1 - c2) <= eps;}
   Float len2() const {return sq((*this)(X)) + sq((*this)(Y)) + sq((*this)(Z));}
   Float len() const {return sqrt(len2());}
-  Vec3 normalize() const {
+  TVec3 normalize() const {
     return (*this)/len();
   }
-  Vec3 normalizeZ(Float l) const { // expects the pre-computed length argument for efficiency
+  TVec3 normalizeZ(Float l) const { // expects the pre-computed length argument for efficiency
     if (l != 0)
       return (*this)/l;
     else
-      return Vec3(0,0,0);
+      return TVec3(0,0,0);
   }
-  Vec3 normalizeZ() const {
+  TVec3 normalizeZ() const {
     return normalizeZ(len());
   }
-  Vec3 normalizeZl(Float outLen) const {
+  TVec3 normalizeZl(Float outLen) const {
     auto l = len();
     outLen = l;
     if (l != 0)
       return (*this)/l;
     else
-      return Vec3(0,0,0);
+      return TVec3(0,0,0);
   }
-  void snapToGrid(const Vec3 &grid) {
-    Vec3 &i = *this;
+  void snapToGrid(const TVec3 &grid) {
+    TVec3 &i = *this;
     snapToGrid(i(X), grid(X));
     snapToGrid(i(Y), grid(Y));
     snapToGrid(i(Z), grid(Z));
@@ -57,78 +58,81 @@ public:
   static void snapToGrid(Float &c, Float g) {
     c = int(c/g)*g;
   }
-  friend std::ostream& operator<<(std::ostream &os, const Vec3 &v) {
+  friend std::ostream& operator<<(std::ostream &os, const TVec3 &v) {
     os << "{" << v(X) << "," << v(Y) << "," << v(Z) << "}";
     return os;
   }
-  Vec3 operator+() const {
+  TVec3 operator+() const {
     return *this;
   }
-  Vec3 operator-() const {
-    const Vec3 &i = *this;
-    return Vec3(-i(X), -i(Y), -i(Z));
+  TVec3 operator-() const {
+    const TVec3 &i = *this;
+    return TVec3(-i(X), -i(Y), -i(Z));
   }
-  Vec3 operator*(Float m) const {
-    const Vec3 &i = *this;
-    return Vec3(i(X)*m, i(Y)*m, i(Z)*m);
+  TVec3 operator*(Float m) const {
+    const TVec3 &i = *this;
+    return TVec3(i(X)*m, i(Y)*m, i(Z)*m);
   }
-  Float operator*(const Vec3 &v) const { // scalar multiplication
-    const Vec3 &i = *this;
+  Float operator*(const TVec3 &v) const { // scalar multiplication
+    const TVec3 &i = *this;
     return i(X)*v(X) + i(Y)*v(Y) + i(Z)*v(Z);
   }
-  Vec3 scale(const Vec3 &v) const { // scalar multiplication
-    const Vec3 &i = *this;
+  TVec3 scale(const TVec3 &v) const { // scalar multiplication
+    const TVec3 &i = *this;
     return {i(X)*v(X), i(Y)*v(Y), i(Z)*v(Z)};
   }
-  Vec3 cross(const Vec3 &v) const { // cross (vector) multiplication
-    const Vec3 &i = *this;
-    return Vec3(-i(Z)*v(Y) + i(Y)*v(Z), i(Z)*v(X) - i(X)*v(Z), -i(Y)*v(X) + i(X)*v(Y)); // see https://en.wikipedia.org/wiki/Cross_product#Conversion_to_matrix_multiplication
+  TVec3 cross(const TVec3 &v) const { // cross (vector) multiplication
+    const TVec3 &i = *this;
+    return TVec3(-i(Z)*v(Y) + i(Y)*v(Z), i(Z)*v(X) - i(X)*v(Z), -i(Y)*v(X) + i(X)*v(Y)); // see https://en.wikipedia.org/wiki/Cross_product#Conversion_to_matrix_multiplication
   }
-  Float angle(const Vec3 &v) const {
+  Float angle(const TVec3 &v) const {
     return std::acos((*this)*v/(this->len()*v.len()));
   }
-  Vec3 operator/(Float m) const {
-    const Vec3 &i = *this;
-    return Vec3(i(X)/m, i(Y)/m, i(Z)/m);
+  TVec3 operator/(Float m) const {
+    const TVec3 &i = *this;
+    return TVec3(i(X)/m, i(Y)/m, i(Z)/m);
   }
-  Vec3 operator+(const Vec3 &v) const {
-    const Vec3 &i = *this;
-    return Vec3(i(X)+v(X), i(Y)+v(Y), i(Z)+v(Z));
+  TVec3 operator+(const TVec3 &v) const {
+    const TVec3 &i = *this;
+    return TVec3(i(X)+v(X), i(Y)+v(Y), i(Z)+v(Z));
   }
-  Vec3 operator-(const Vec3 &v) const {
-    const Vec3 &i = *this;
-    return Vec3(i(X)-v(X), i(Y)-v(Y), i(Z)-v(Z));
+  TVec3 operator-(const TVec3 &v) const {
+    const TVec3 &i = *this;
+    return TVec3(i(X)-v(X), i(Y)-v(Y), i(Z)-v(Z));
   }
-  Vec3& operator+=(const Vec3 &v) {
-    Vec3 &i = *this;
+  TVec3& operator+=(const TVec3 &v) {
+    TVec3 &i = *this;
     i(X) += v(X);
     i(Y) += v(Y);
     i(Z) += v(Z);
     return *this;
   }
-  Vec3& operator-=(const Vec3 &v) {
-    Vec3 &i = *this;
+  TVec3& operator-=(const TVec3 &v) {
+    TVec3 &i = *this;
     i(X) -= v(X);
     i(Y) -= v(Y);
     i(Z) -= v(Z);
     return *this;
   }
-  Vec3 project(const Vec3 &dir) const { // dir is assumed to be a normal vector or zero vector
+  TVec3 project(const TVec3 &dir) const { // dir is assumed to be a normal vector or zero vector
     return dir*((*this)*dir);
   }
-  Vec3 orthogonal(const Vec3 &dir) const { // dir is assumed to be a normal vector or zero vector
+  TVec3 orthogonal(const TVec3 &dir) const { // dir is assumed to be a normal vector or zero vector
     return *this - project(dir);
   }
   bool isNormalized() const {return is(len2(), 1);} // to be used primarily in asserts, otherwie this check probably shouldn't be needed
-  bool isParallel(const Vec3 &other) const {return is(normalize()*other.normalize(), 1);}
-  bool isOrthogonal(const Vec3 &other) const {return is(cross(other).len2(), 1);}
-  Vec3 divOneByOne(const Vec3 &d) const { // dir is assumed to be a normal vector or zero vector
-    const Vec3 &i = *this;
-    return Vec3(i(X)/d(X), i(Y)/d(Y), i(Z)/d(Z));
+  bool isParallel(const TVec3 &other) const {return is(normalize()*other.normalize(), 1);}
+  bool isOrthogonal(const TVec3 &other) const {return is(cross(other).len2(), 1);}
+  TVec3 divOneByOne(const TVec3 &d) const { // dir is assumed to be a normal vector or zero vector
+    const TVec3 &i = *this;
+    return TVec3(i(X)/d(X), i(Y)/d(Y), i(Z)/d(Z));
   }
   static bool is(Float f1, Float f2) {return std::abs(f1-f2) < 0.001;} // tests how close are numbers
   static Float radToDeg(Float r) {return r/M_PI*180;}
   static Float degToRad(Float d) {return d/180*M_PI;}
 private:
   static Float sq(Float f) {return f*f;}
-};
+}; // TVec3
+
+typedef TVec3<double> Vec3;
+typedef TVec3<float> Vec3f;
