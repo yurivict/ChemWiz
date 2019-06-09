574d06b99e5319ed64887d45466a4381bb9deb04
Yuri <yuri@rawbw.com>
2019-06-09 11:46:25 -0700

    Add semicolons to the in-C++ defined JS code.

--- js-support.cpp
+++ js-support.cpp
@@ -101,24 +101,24 @@ void JsSupport::addNsFunctionJs(js_State *J, const char *nsNameStr, const char *
 void JsSupport::registerFuncRequire(js_State *J) {
   js_dostring(J,
     "function require(name) {\n"
-    "  var cache = require.cache\n"
+    "  var cache = require.cache;\n"
     "  if (name in cache)\n"
-    "    return cache[name]\n"
-    "  var exports = {}\n"
-    "  cache[name] = exports\n"
-    "  Function('exports', File.read(name.indexOf('/')==-1 ? 'modules/'+name+'.js' : name+'.js'))(exports)\n"
-    "  return exports\n"
+    "    return cache[name];\n"
+    "  var exports = {};\n"
+    "  cache[name] = exports;\n"
+    "  Function('exports', File.read(name.indexOf('/')==-1 ? 'modules/'+name+'.js' : name+'.js'))(exports);\n"
+    "  return exports;\n"
     "}\n"
-    "require.cache = Object.create(null)\n"
+    "require.cache = Object.create(null);\n"
   );
 }
 
 void JsSupport::registerFuncImportCodeString(js_State *J) {
   js_dostring(J,
     "function importCodeString(codeString) {\n"
-    "  var exports = {}\n"
-    "  Function('exports', codeString)(exports)\n"
-    "  return exports\n"
+    "  var exports = {};\n"
+    "  Function('exports', codeString)(exports);\n"
+    "  return exports;\n"
     "}\n"
   );
 }
