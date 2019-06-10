f44e645011a1d89545999a7fdd009ed2bebc039a
Yuri <yuri@rawbw.com>
2019-06-10 02:00:42 -0700

    Implement a few basic WebGL functions. Implement GlfwWindow. Simplest WebGL program now works.

--- glfw-window.cpp
+++ glfw-window.cpp
@@ -3,11 +3,14 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <memory>
 
 #include "js-support.h"
 #include "xerror.h"
 
-static const char *TAG_GlfwWindow    = "GlfwWindow";
+// see the GLFW sample app: https://github.com/skaslev/gl3w/blob/master/src/glfw_test.c
+
+const char *TAG_GlfwWindow    = "GlfwWindow"; // used from outside
 
 class GlfwInit {
 public:
@@ -22,57 +25,101 @@ public:
 
 static GlfwInit __glfwInit__;
 
-class GlfwWindow {
-  GLFWwindow* window;
+//
+// GlRunner - runs some glXx commands when called
+//
+class GlRunner {
 public:
-  GlfwWindow() {
+  virtual ~GlRunner() { }
+  virtual void run(GLFWwindow* window) = 0;
+}; // GlRunner
+
+class MyTestGlCode : public GlRunner {
+public:
+  ~MyTestGlCode() { }
+  void run(GLFWwindow* window) {
+    float ratio;
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    ratio = width / (float) height;
+    glViewport(0, 0, width, height);
+    //
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.f, 0.f, 0.f);
+    glVertex3f(-0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 1.f, 0.f);
+    glVertex3f(0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.f, 0.6f, 0.f);
+    glEnd();
+    //
+    glfwSwapBuffers(window);
+  }
+}; // MyTestGlCode
+
+//
+// JsFunctionGlRunner calls arguments that are on stack: (function glFunc(gl), glIface)
+//
+class JsFunctionGlRunner : public GlRunner {
+  js_State *J;
+public:
+  JsFunctionGlRunner(js_State *newJ) : J(newJ) { }
+  ~JsFunctionGlRunner() { }
+  void run(GLFWwindow* window) {
+    // XXX repeat
+    float ratio;
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    ratio = width / (float) height;
+    glViewport(0, 0, width, height);
+    // call the JS function
+    js_copy(J, -2);      // push function
+    js_pushundefined(J); // this
+    js_copy(J, -3);      // push the argument
+    js_call(J, 1); // call glFunc(glIface)
+    js_pop(J, 1);  // remove the result which is supposed to be 'undefined'
+    // XXX repeat
+    glfwSwapBuffers(window);
+  }
+}; // JsFunctionGlRunner
+
+class GlfwWindow {
+  GLFWwindow *window;
+  std::unique_ptr<GlRunner>   glRunner;
+public:
+  GlfwWindow() : glRunner(new MyTestGlCode) {
     window = glfwCreateWindow(640, 480, "Simple Window", NULL, NULL);
     if (!window) {
       glfwTerminate();
       ERROR("GLFW: Window or OpenGL context creation failed")
     }
 
+    glfwSetWindowUserPointer(window, this);
+
     glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
       std::cout << "GLFW: key pressed: key=" << key << std::endl;
       if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
     });
-
-    display();
+    glfwSetWindowRefreshCallback(window, [](GLFWwindow* window) {
+      ((GlfwWindow*)glfwGetWindowUserPointer(window))->display(window);
+    });
   }
   ~GlfwWindow() {
     if (!window)
       glfwDestroyWindow(window);
   }
-private:
-  void display() {
-    glfwMakeContextCurrent(window);
-    while (!glfwWindowShouldClose(window)) {
-      float ratio;
-      int width, height;
-      glfwGetFramebufferSize(window, &width, &height);
-      ratio = width / (float) height;
-      glViewport(0, 0, width, height);
-      glClear(GL_COLOR_BUFFER_BIT);
-      glMatrixMode(GL_PROJECTION);
-      glLoadIdentity();
-      glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-      glMatrixMode(GL_MODELVIEW);
-      glLoadIdentity();
-      glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-      glBegin(GL_TRIANGLES);
-      glColor3f(1.f, 0.f, 0.f);
-      glVertex3f(-0.6f, -0.4f, 0.f);
-      glColor3f(0.f, 1.f, 0.f);
-      glVertex3f(0.6f, -0.4f, 0.f);
-      glColor3f(0.f, 0.f, 1.f);
-      glVertex3f(0.f, 0.6f, 0.f);
-      glEnd();
-      glfwSwapBuffers(window);
-      glfwPollEvents();
-    }
-    glfwDestroyWindow(window);
-    window = nullptr;
+  void setGlRunner(GlRunner *newGlRunner) {glRunner.reset(newGlRunner);}
+  GLFWwindow* getWindow() {return window;}
+  void display(GLFWwindow* window) {
+    glRunner->run(window);
   }
   static void keyPressed(unsigned char key, int x, int y) {  
     if (key == 'y') {
@@ -100,7 +147,20 @@ void init(js_State *J) {
     AssertNargs(0)
     ReturnObj(new GlfwWindow);
   });
-  // no methods are defined for GlfwWindow yet
+  ADD_METHOD_CPP(GlfwWindow, loop, { // arguments: (function glFunc(gl), glIface) to pass to glFunc
+    AssertNargs(2)
+    printf("OpenGL %s, GLSL %s\n", glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
+    auto This = GetArg(GlfwWindow, 0);
+    auto window = This->getWindow();
+    This->setGlRunner(new JsFunctionGlRunner(J));
+    // >>> hard loop
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+      This->display(window);
+    }
+    glfwDestroyWindow(window); // <<< hard loop
+  }, 2);
   JsSupport::endDefineClass(J);
 }
 
--- webgl.cpp
+++ webgl.cpp
@@ -10,6 +10,11 @@
 
 #include "js-support.h"
 
+//#include <iostream>
+#include <string>
+
+#include <GL/gl.h>
+
 static const char *TAG_WebGlInterface    = "WebGlInterface";
 
 namespace JsBinding {
@@ -22,10 +27,34 @@ void init(js_State *J) {
     // WebGlInterface is a simple JS object with pre-defined set of methods
     js_newobject(J);
     {
-      js_pushnumber(J, 10);
-      js_setproperty(J, -2, "fld1");
-      js_pushnumber(J, 101);
-      js_setproperty(J, -2, "fld2");
+#define GL_CONST(gl_const) \
+      js_pushnumber(J, GL_##gl_const); \
+      js_setproperty(J, -2, #gl_const);
+
+#define GL_FUNC(funcNameWGL, nargs, code) \
+      js_newcfunction(J, [](js_State *J) code, #funcNameWGL, nargs); \
+      js_setproperty(J, -2, #funcNameWGL);
+
+      // functions
+      GL_FUNC(viewport, 4, {
+        glViewport(GetArgInt32(1), GetArgInt32(2), GetArgInt32(3), GetArgInt32(4));
+      });
+      GL_FUNC(clear, 1, {
+        //std::cout << "clear: " << GetArgFloat(1) << std::endl;
+        glClear(GetArgInt32(1));
+      });
+      GL_FUNC(clearColor, 4, {
+        //std::cout << "clearColor: " << GetArgFloat(1) << " " << GetArgFloat(2) << " " << GetArgFloat(3) << " " << GetArgFloat(4) << std::endl;
+        glClearColor(GetArgFloat(1), GetArgFloat(2), GetArgFloat(3), GetArgFloat(4));
+      });
+
+      // consts
+      GL_CONST(COLOR_BUFFER_BIT)
+      GL_CONST(DEPTH_BUFFER_BIT)
+      GL_CONST(TRIANGLES)
+
+#undef GL_FUNC
+#undef GL_CONST
     }
   });
   // no methods are defined for WebGlInterface
