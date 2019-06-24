e0f47cbe63bed37b4b51a29384034d55bc9be08b
Yuri <yuri@rawbw.com>
2019-06-24 02:20:18 -0700

    Add the module ShowLocalWebApp allowing to show the local web app in the local browser process.

---
+++ modules/show-local-web-app.js
@@ -0,0 +1,14 @@
+// module ShowLocalWebApp: open the browser and navigate to the local web app
+
+var httpServeDirectory = require("http-serve-directory");
+
+
+//
+// exports
+//
+
+exports.showDir = function(dir) {
+  // BUG: need to be able to wait for the browser process and to stop the http server once the browser is done
+  Process.system("daemon qt5-QtWebEngine-browser/browser http://localhost:8181 2>/dev/null"); // XXX race condition: the app can open before the http server
+  httpServeDirectory.serve("contrib/js-libs/pv", 8181);
+};
