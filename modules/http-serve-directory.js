ff4e14e39247669acb59107b3b138c13fb01045d
Yuri <yuri@rawbw.com>
2019-06-23 23:58:38 -0700

    Add the HttpServeDirectory module that serves directory contents over http.

---
+++ modules/http-serve-directory.js
@@ -0,0 +1,70 @@
+// module HttpServeDirectory: serves the directgory through http
+
+var fs = require('fs');
+var http = require('http');
+
+//
+// helpers
+//
+
+function log(msg) {
+  //print("LOG(HttpServeDirectory): "+msg);
+}
+
+function extname(filePath) {
+  var dot = filePath.lastIndexOf('.');
+  return dot != -1 ? filePath.substring(dot+1, filePath.length) : "";
+}
+
+function page404(url) {
+  return "<html>\n"+
+         "  <body>"+
+         "    <h2>Requested file not found: "+url+"</h2>"+
+         "  </body>"+
+         "</html>";
+}
+
+var extToContentType = {
+  'js':   'text/javascript',
+  'css':  'text/css',
+  'json': 'application/json',
+  'png':  'image/png',
+  'jpg':  'image/jpg',
+  'wav':  'audio/wav'
+};
+
+function pathToContentType(filePath) {
+  var contentType = extToContentType[extname(filePath)];
+  if (contentType)
+    return contentType;
+  else
+    return 'text/html';
+}
+
+//
+// exports
+//
+
+exports.serve = function(dir, port) {
+  http.createServer(function (request, response) {
+    log(">>> REQ url="+request.url);
+    var filePath = request.url;
+    if (filePath == '/')
+      filePath = '/index.html';
+    if (filePath.charAt(0) != '/')
+      filePath = '/'+filePath;
+    filePath = dir+filePath;
+
+    fs.readFile(filePath, function(error, content) {
+      if (!error) {
+        log("<<< REQ url="+request.url+" -> ct="+pathToContentType(filePath)+" file="+filePath+" nbytes="+content.length);
+        response.writeHead(200, {'Content-Type': pathToContentType(filePath)});
+        response.end(content, 'utf-8');
+      } else {
+        log("<<< REQ url="+request.url+" -> 404");
+        response.writeHead(404, {'Content-Type': 'text/html'});
+        response.end(page404(request.url));
+      }
+    });
+  }).listen(port);
+};
