3156fb2a4ff95a8541bb80e4a7f7dd7bbc794767
Yuri <yuri@rawbw.com>
2019-06-24 02:49:04 -0700

    Fix dir location in ShowLocalWebApp

--- modules/show-local-web-app.js
+++ modules/show-local-web-app.js
@@ -10,5 +10,5 @@ var httpServeDirectory = require("http-serve-directory");
 exports.showDir = function(dir) {
   // BUG: need to be able to wait for the browser process and to stop the http server once the browser is done
   Process.system("daemon qt5-QtWebEngine-browser/browser http://localhost:8181 2>/dev/null"); // XXX race condition: the app can open before the http server
-  httpServeDirectory.serve("contrib/js-libs/pv", 8181);
+  httpServeDirectory.serve(dir, 8181);
 };
