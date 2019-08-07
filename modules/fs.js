397bfa2a290592415ff4db159935768780b034d5
Yuri <yuri@rawbw.com>
2019-08-07 10:09:48 -0700

    Additions to the 'fs' module.

--- modules/fs.js
+++ modules/fs.js
@@ -2,9 +2,40 @@
 
 // documentation: https://nodejs.org/api/fs.html#fs_file_system
 
+// TODO FIX error object so that it has this structure: "errno":-2,"code":"ENOENT","syscall":"open","path":"xxxxx"
+// docs:
+// * promises API: https://nodejs.org/dist/latest-v10.x/docs/api/fs.html#fs_fs_promises_api (require('fs').promises)
 
 // params
 var paramFileReadStep = 1024*1024; // 1MB
+
+//
+// consts
+//
+
+var errnoToCode = {
+  1:  "EPERM",
+  2:  "ENOENT",
+  3:  "ESRCH",
+  4:  "EINTR",
+  5:  "EIO",
+  12: "ENOMEM",
+  13: "EACCES",
+  17: "EEXIST"
+  // TODO the rest
+};
+
+var errnoToDescr = {
+  1:  "Operation not permitted",
+  2:  "No such file or directory",
+  3:  "No such process",
+  4:  "Interrupted system call",
+  5:  "Input/output error",
+  12: "Cannot allocate memory",
+  13: "Permission denied",
+  17: "File exists"
+  // TODO the rest
+};
 
 //
 // helpers
@@ -17,6 +48,13 @@ function resToCb(res, cb) {
     cb(System.strerror(System.errno()));
 }
 
+function mkErr(syscall, path) {
+  var errno = System.errno();
+  var code = errnoToCode[errno];
+  if (!code)
+    throw "TODO error code for errno="+errno+" is not known";
+  return {"errno": errno, "code": code, "syscall": syscall, "path": path};
+}
 
 //
 // exports
@@ -52,3 +90,38 @@ exports.readFile = function readFile(path, cb) {
     }
   }
 };
+
+exports.lstat = function(path, callback) { // fs.lstat(path[, options], callback): https://nodejs.org/dist/latest-v10.x/docs/api/fs.html#fs_fs_lstat_path_options_callback
+  var s = FileApi.lstat(path);
+  if (typeof s === "object")
+    callback(undefined, s);
+  else
+    callback(mkErr("lstat", path));
+};
+
+exports.lstatSync = function(path) {
+  var s = FileApi.lstat(path);
+  if (typeof s === "object")
+    return s;
+  else
+    throw "lstat failed: path="+path+" error="+errnoToCode[System.errno()];
+};
+
+exports.stat = function(path, callback) {
+  var s = FileApi.stat(path);
+  if (typeof s === "object")
+    callback(undefined, s);
+  else
+    callback(mkErr("stat", path));
+};
+
+exports.statSync = function(path) {
+  var s = FileApi.stat(path);
+  if (typeof s === "object")
+    return addStatsMethods(s);
+  else {
+    var errno = System.errno();
+    throw errnoToCode[errno]+": "+errnoToDescr[errno].toLowerCase()+", stat '"+path+"'";
+  }
+};
+
