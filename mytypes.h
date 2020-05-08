77832401a5eedd9337ddf3824ceb80d6946ff205
Yuri <yuri@rawbw.com>
2020-05-07 23:36:05 -0700

    Add the forgotten NeuralNetwork class.

--- mytypes.h
+++ mytypes.h
@@ -8,3 +8,11 @@ typedef double Float;
 
 // Binary is a JS-bound byte array
 typedef std::vector<uint8_t> Binary;
+
+// Neural Network: to avoid including MiniDNN.h we use a forward declaration
+namespace MiniDNN {
+class Network;
+}
+
+typedef MiniDNN::Network NeuralNetwork;
+
